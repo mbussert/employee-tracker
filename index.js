8a933c258bbea38e31c8bb71bff24f7ad666b426
Mike Bussert <mbussert@gmail.com>
2021-04-12 13:22:38 -0400

    Setup DOTENV

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 require("dotenv").config();
+const mysql = require("mysql");
 
-const db = require("db");
-db.connect({
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+
+    port: process.env.DB_PORT,
+
+    user: process.env.DB_USER,
+
+    password: process.env.DB_PASS,
+    database: process.env.DATABASE,
+});
+
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`connected as id ${connection.threadId}`);
+  // querySongArtist();
 });
