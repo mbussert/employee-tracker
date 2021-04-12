7e5293161c7175bce7a46c34e7eed792132ca69e
Mike Bussert <mbussert@gmail.com>
2021-04-12 16:56:17 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 require("dotenv").config();
 const mysql = require("mysql");
+const cTable = require("console.table");
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-
-    port: process.env.DB_PORT,
-
-    user: process.env.DB_USER,
-
-    password: process.env.DB_PASS,
-    database: process.env.DATABASE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DATABASE,
 });
-
 
 connection.connect((err) => {
   if (err) throw err;
